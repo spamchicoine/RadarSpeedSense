54e8eb1904cd8d5bf9ab84299513b95c2b108b9e
spamchicoine <114253734+spamchicoine@users.noreply.github.com>
2024-11-13 17:08:17 -0500

    Time stamping
    Added time stamping functionality, only missing an hourly sync which can be easily added by listening for when minutes > 59 then calling the configtime on timestruct. Contains a decent amount of hardcoded values atm.

--- speed_sense_radar_serial_comm/speed_sense_radar_serial_comm.ino
+++ speed_sense_radar_serial_comm/speed_sense_radar_serial_comm.ino
@@ -1,11 +1,21 @@
+#include "WiFi.h"
+#include "time.h"
+
 const int DECODER0 = D0;
 const int DECODER1 = D1;
 const int DECODER2 = D2;
 const int DECODER3 = D3;
+
+char values[1000];
  
-int speed = 0;
-int d1 = 0;
-int d2 = 0;
+int speed, d1, d2;
+unsigned long ltime, millidif;
+
+struct tm timeinfo;
+size_t maxsize = 20;
+
+const char *ssid = "Verizon-SM-N970U-4a7d";
+const char *password = "uqfw684*";
 
 int dec_bin[][4] = {{0,0,0,0},
                     {0,0,0,1},
@@ -21,9 +31,21 @@ int dec_bin[][4] = {{0,0,0,0},
 void setup() {
   Serial.begin(19200);
   while (!Serial);
-  Serial.println("SI");
+  delay(5);
 
-  // put your setup code here, to run once:
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("\n");
+
+  configTime(0, 0, "pool.ntp.org");
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+
   pinMode(DECODER0, OUTPUT);
   pinMode(DECODER1, OUTPUT);
   pinMode(DECODER2, OUTPUT);
@@ -32,24 +54,45 @@ void setup() {
 }
 
 void loop() {
+  TimeHandle(setsevseg());
+}
 
-  speed = Serial.readString().toInt();
-
-  if(speed < 0){
-    speed *= -1;
+void TimeHandle(int speed) {
+  ltime = (unsigned long)(millis() - millidif);
+  if (ltime >= 1000){
+    millidif = millidif + ltime;
+    ltime = 0;
+    if (59 < ++timeinfo.tm_sec){
+      timeinfo.tm_sec = 0;
+      if (59 < ++timeinfo.tm_min){
+        timeinfo.tm_min = 0;
+        timeinfo.tm_hour++;
+      }
+    }
+    char timestamp[20];
+    strftime(timestamp, maxsize, "%D %T", &timeinfo);
+    Serial.print(timestamp);
+    if (speed < 10){
+      Serial.print(" ");
+      Serial.println(speed);
+    }
   }
+}
 
-  // put your main code here, to run repeatedly:
+int setsevseg() {
+  
+  if (Serial.available()){
+    speed = Serial.readString().toInt();
+  }
   d2 = speed % 10;
-
   if (speed > 9){
     d1 = speed / 10;
   }
-
   // set outputs to decoder (only 2nd digit)
   digitalWrite(DECODER0, dec_bin[d2][0]);
   digitalWrite(DECODER1, dec_bin[d2][1]);
   digitalWrite(DECODER2, dec_bin[d2][2]);
   digitalWrite(DECODER3, dec_bin[d2][3]);
 
+  return speed;
 }
