bd79fa0e13796cf810c974677f3f4a726f4173a0
spamchicoine <114253734+spamchicoine@users.noreply.github.com>
2025-03-14 19:19:46 -0400

    Working Revision
    This code works fairly consistently with the sleep call disabled and semi consistently with the sleep call disabled.

--- speed_sense_radar_serial_comm/speed_sense_radar_serial_comm.ino
+++ speed_sense_radar_serial_comm/speed_sense_radar_serial_comm.ino
@@ -1,9 +1,15 @@
 #include <Arduino.h>
 #include <driver/uart.h>
 
-#include "WiFi.h"
 #include "time.h"
 #include "wifi_setup.h"
+
+extern HTTPClient client;
+extern WebServer server;
+
+bool isConnectedToWiFi = false;
+bool debug = true;
+String test_data = "03/12/25-00:47:33 122 11\n";
 
 // Digit 1
 const int DECODER0 = D1;
@@ -24,14 +30,13 @@ String values = String("");
 int speed, d1, d2;
 const int SPEED_LIMIT = 25;
 
+int STAMP_FLAG = 0;
+int TIME_SINCE_VAL;
+
 // Values for keeping track of time
 struct tm timeinfo;
 size_t maxsize = 20;
 unsigned long ltime, millidif;
-
-// Values for keeping track of sleeping
-int TIME_SINCE_VAL = 0;
-int uMS_S_FACTOR = 1000000;
 
 // Values for keeping track of flashing the digits
 unsigned long flash_time, flash_millidif;
@@ -51,20 +56,17 @@ int dec_bin[][4] = {{0,0,0,0},
                     {1,0,1,0}}; // 10 for blank
 
 void setup() {
-  // Begin Serial connection with Radar
-  // If USB CDC on boot is disabled Serial defaults to RX/TX UART
-  Serial.begin(115200);
-  while (!Serial);
-  delay(2000);
   
-  // Begin WiFi connection with client
-  setup_wifi();
-
-  // Sync time with ntp server
-  configTime(0, 0, "pool.ntp.org");
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("Failed to obtain time");
-    return;
+  
+  WiFi.mode(WIFI_OFF);
+  if(debug) {
+    isConnectedToWiFi = connectToWiFi("MS7549", "t88-24O6");
+  } else {
+    setup_mode();
+    while(!isConnectedToWiFi) {
+      delay(500);
+      server.handleClient();
+    }
   }
 
   // Set BCD control pins to outputs
@@ -87,19 +89,45 @@ void setup() {
   digitalWrite(DECODER6, dec_bin[10][2]);
   digitalWrite(DECODER7, dec_bin[10][3]);
 
-  // Sleep setup
+  // Begin Serial connection with Radar
+  // If USB CDC on boot is disabled Serial defaults to RX/TX UART
+  Serial.begin(115200);
+  while (!Serial);
+  delay(1000);
+
+  // send_SpeedData(test_data);
+  // Sync time with ntp server
+  configTime(0, 0, "pool.ntp.org");
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+
+  char timestamp[20];
+  strftime(timestamp, 20, "%D-%T", &timeinfo);
+  values.concat(String(timestamp));
+
+  // GPIO 20 is the default UART_0 RX pin
   gpio_sleep_set_direction(GPIO_NUM_20, GPIO_MODE_INPUT);
   gpio_sleep_set_pull_mode(GPIO_NUM_20, GPIO_PULLUP_ONLY);
 
   uart_set_wakeup_threshold(UART_NUM_0, 3);   // 3 edges on U0RXD to wakeup
   esp_sleep_enable_uart_wakeup(UART_NUM_0);   // Enable UART 0 as wakeup source
 
-  Serial.flush();
+  gpio_sleep_set_pull_mode(GPIO_NUM_3, GPIO_PULLUP_ONLY);
+  gpio_sleep_set_pull_mode(GPIO_NUM_4, GPIO_PULLDOWN_ONLY);
+  gpio_sleep_set_pull_mode(GPIO_NUM_5, GPIO_PULLUP_ONLY);
+  gpio_sleep_set_pull_mode(GPIO_NUM_2, GPIO_PULLDOWN_ONLY);
+
+  gpio_sleep_set_pull_mode(GPIO_NUM_10, GPIO_PULLUP_ONLY);
+  gpio_sleep_set_pull_mode(GPIO_NUM_9, GPIO_PULLDOWN_ONLY);
+  gpio_sleep_set_pull_mode(GPIO_NUM_8, GPIO_PULLUP_ONLY);
+  gpio_sleep_set_pull_mode(GPIO_NUM_7, GPIO_PULLDOWN_ONLY);
+
 }
 
 void loop() {
   
-  // Update Time
   ltime = (unsigned long)(millis() - millidif);
 
   if (ltime >= 1000){
@@ -110,29 +138,56 @@ void loop() {
     if (59 < ++timeinfo.tm_sec){
       timeinfo.tm_sec = 0;
 
+      TIME_SINCE_VAL = 0;
+
+      if (values.length() > 0){
+        values.concat("\n");
+        send_SpeedData(values);
+        values = String("");
+      }
+      STAMP_FLAG = 1;
+
       if (59 < ++timeinfo.tm_min){
+
         timeinfo.tm_min = 0;
         configTime(0, 0, "pool.ntp.org");
+
         if(!getLocalTime(&timeinfo)){
           Serial.println("Failed to obtain time");
           return;
         }
-        send_SpeedData(values);
-        values = String("");
-        char timestamp[maxsize];
-        strftime(timestamp, maxsize, "%D-%T", &timeinfo);
-        values.concat(String(timestamp));
+
+        TIME_SINCE_VAL = 0;
+        if (values.length() > 0){
+          values.concat("\n");
+          send_SpeedData(values);
+          values = String("");
+        }
+        STAMP_FLAG = 1;
       }
     }
   }
   
+  flash_time = (unsigned long)(millis() - flash_millidif);
+
+  if (flash_time >= 200){
+        flash_millidif = flash_millidif + flash_time;
+        flash_time = 0;
+        flash_flag = (flash_flag + 1) % 2;
+  }
+  
   // Handle data from radar
   if (Serial.available()){
-    // Reset sleep counter
-    TIME_SINCE_VAL = 0;
 
     // returns 0 if invalid, radar will have minimum threshold above this anyways
     speed = Serial.readString().toInt();
+
+    if (STAMP_FLAG == 1){
+      char timestamp[20];
+      strftime(timestamp, 20, "%D-%T", &timeinfo);
+      values.concat(String(timestamp));
+      STAMP_FLAG = 0;
+    }
     
     // assign digit values
     if (0 == speed){
@@ -143,81 +198,43 @@ void loop() {
       d1 = speed % 10;
       d2 = speed / 10;
     }
-    
+
     // Add reading to values
     values.concat(" "+String(speed));
+    TIME_SINCE_VAL = 0;
+  } 
+
+  if (TIME_SINCE_VAL <= 3){
+    digitalWrite(DECODER0, dec_bin[d2][0]);
+    digitalWrite(DECODER1, dec_bin[d2][1]);
+    digitalWrite(DECODER2, dec_bin[d2][2]);
+    digitalWrite(DECODER3, dec_bin[d2][3]);
     
-    // Handle flashing of digits
-    flash_time = (unsigned long)(millis() - flash_millidif);
-
-    if (flash_time >= 50){
-        flash_millidif = flash_millidif + flash_time;
-        flash_time = 0;
-        flash_flag = (flash_flag + 1) % 2;
-    }
-    // Set digits
-    if (speed > SPEED_LIMIT){
-      if (flash_flag == 1) {
-        digitalWrite(DECODER0, dec_bin[d1][0]);
-        digitalWrite(DECODER1, dec_bin[d1][1]);
-        digitalWrite(DECODER2, dec_bin[d1][2]);
-        digitalWrite(DECODER3, dec_bin[d1][3]);
-        
-        digitalWrite(DECODER4, dec_bin[d2][0]);
-        digitalWrite(DECODER5, dec_bin[d2][1]);
-        digitalWrite(DECODER6, dec_bin[d2][2]);
-        digitalWrite(DECODER7, dec_bin[d2][3]);
-      }
-      else {
-        digitalWrite(DECODER0, dec_bin[10][0]);
-        digitalWrite(DECODER1, dec_bin[10][1]);
-        digitalWrite(DECODER2, dec_bin[10][2]);
-        digitalWrite(DECODER3, dec_bin[10][3]);
-
-        digitalWrite(DECODER4, dec_bin[10][0]);
-        digitalWrite(DECODER5, dec_bin[10][1]);
-        digitalWrite(DECODER6, dec_bin[10][2]);
-        digitalWrite(DECODER7, dec_bin[10][3]);
-      }
-    }
-    else{
-      digitalWrite(DECODER0, dec_bin[d1][0]);
-      digitalWrite(DECODER1, dec_bin[d1][1]);
-      digitalWrite(DECODER2, dec_bin[d1][2]);
-      digitalWrite(DECODER3, dec_bin[d1][3]);
-
-      digitalWrite(DECODER4, dec_bin[d2][0]);
-      digitalWrite(DECODER5, dec_bin[d2][1]);
-      digitalWrite(DECODER6, dec_bin[d2][2]);
-      digitalWrite(DECODER7, dec_bin[d2][3]);
-    }
+    digitalWrite(DECODER4, dec_bin[d1][0]);
+    digitalWrite(DECODER5, dec_bin[d1][1]);
+    digitalWrite(DECODER6, dec_bin[d1][2]);
+    digitalWrite(DECODER7, dec_bin[d1][3]);
   }
-  // No data -> new line and timestamp
   else{
-    
-    char timestamp[maxsize];
-    strftime(timestamp, maxsize, "%D-%T", &timeinfo);
-    values.concat("\n"+String(timestamp));
     
     digitalWrite(DECODER0, dec_bin[10][0]);
     digitalWrite(DECODER1, dec_bin[10][1]);
     digitalWrite(DECODER2, dec_bin[10][2]);
     digitalWrite(DECODER3, dec_bin[10][3]);
+    
     digitalWrite(DECODER4, dec_bin[10][0]);
     digitalWrite(DECODER5, dec_bin[10][1]);
     digitalWrite(DECODER6, dec_bin[10][2]);
     digitalWrite(DECODER7, dec_bin[10][3]);
-  }
-  // Handle sleep
-  if (5 <= TIME_SINCE_VAL){
-    TIME_SINCE_VAL = 0;
-    esp_light_sleep_start();
-    Serial.readString();
-    // Light sleep will resume here
-    // setup_wifi(); // Not sure if this is needed
-  }
-}
 
-void config() {
-  //TO DO
+    if (values.length() > 0){
+      values.concat("\n");
+      send_SpeedData(values);
+      values = String("");
+    }
+    
+    STAMP_FLAG = 1;
+
+    //esp_light_sleep_start();
+  }
 }
--- speed_sense_radar_serial_comm/wifi_setup.cpp
+++ speed_sense_radar_serial_comm/wifi_setup.cpp
@@ -1,60 +1,71 @@
 #include "wifi_setup.h"
 #include "string.h"
 
-
 int    HTTP_PORT   = 80;
 String HTTP_METHOD = "POST"; // or "POST"
-String HOST_NAME = "http://speedsensesdp.pagekite.me"; // hostname of web server:
+String HOST_NAME = "http://speedsense.pagekite.me"; // hostname of web server:
 String PATH_NAME   = "/save/";
 int    INST_ID     = 0;
 
 // Replace with your network credentials
-const char* ssid     = "MS7549";
-const char* password = "t88-24O6";
-//const char *ssid = "Verizon-SM-N970U-4a7d";
-//const char *password = "uqfw684*";
+String ServerSSID     = "SpeedSense";
+String ServerPassword = "password";
+
+const IPAddress localIP(192, 168, 4, 1); // Your desired static IP
+const IPAddress gateway(192, 168, 1, 1);  // Network gateway
+const IPAddress subnet(255, 255, 255, 0); // Subnet mask
 
 HTTPClient client;
+WebServer server(80);
+extern bool isConnectedToWiFi;
 
-void setup_wifi(){
-    delay(2000);
-    Serial.begin(115200);
-    delay(1000);
+String extractValue(String data, String fieldName) {
+  int startIdx = data.indexOf("name=\"" + fieldName + "\"");
+  if (startIdx == -1) return "";
 
-    // Connect to Wi-Fi
-    WiFi.begin(ssid, password);
-    
-    while (WiFi.status() != WL_CONNECTED) {
-        Serial.println("Connecting...");
-        delay(1000);
-    }
-    Serial.println("Connected");
+  startIdx = data.indexOf("\r\n\r\n", startIdx);
+  if (startIdx == -1) return "";
+
+  startIdx += 4;  // Move past the line break
+  int endIdx = data.indexOf("\r\n", startIdx);
+  if (endIdx == -1) return "";
+
+  return data.substring(startIdx, endIdx);
 }
 
-void send_SpeedData(String Data) {
-  // Example string format:
-  if(WiFi.status() == WL_CONNECTED){
-    // start connection
-    client.begin(HOST_NAME+PATH_NAME);
-    // add headers
-    client.addHeader("Content-Type", "text/plain");
-    client.addHeader("Connection", "close");
-    client.addHeader("direction", "0");
-    client.addHeader("custom-text", "TEST LIVE");
+void handlePost() {
+  String targetSSID = "";
+  String targetPassword = "";
 
-    // Send POST request
-    int httpResponseCode = client.POST(Data);
+  if (server.hasArg("plain")) {
+    String postBody = server.arg("plain");  // Get the POST data
+    //Serial.println("Received POST data:");
+    //Serial.println(postBody);
+    
+    targetSSID = extractValue(postBody, "ssid");
+    //Serial.println("Extracted SSID: " + targetSSID);
+    targetPassword = extractValue(postBody, "pwd");
+    //Serial.println("Extracted Password: " + targetPassword);
 
-    // Check the HTTP response
-    if (httpResponseCode > 0) {
-      String response = client.getString();
-    }
-    client.end();
+    server.sendHeader("Access-Control-Allow-Origin", "*");
+    server.send(200, "application/json", "{\"status\":\"success\"}");
+
+    // Attempt to connect to WiFi
+    isConnectedToWiFi = connectToWiFi(targetSSID, targetPassword);
+    //Serial.print("IP Address: ");
+    //Serial.println(WiFi.localIP());  // Print assigned IP
+
+  } else {
+    server.sendHeader("Access-Control-Allow-Origin", "*");
+    server.send(400, "application/json", "{\"error\":\"No data received\"}");
   }
 }
 
-void send_TestData(String Data) {
+void send_SpeedData(String Data) {
+  
   // Example string format:
+  //String test_data = "2024-11-06-12-31-42 25.8 25.8 25.7 25.8 25.5 25.8 25.7 25.6 25.6 25.7 25.5 25.8 25.7 25.5 25.5 25.7 25.5 25.4 25.4 25.7 25.4 25.3 25.6 25.6 25.5 25.2 25.5 25.3 25.3 25.6 25.4 25.4 25.5 25.5 25.5 25.2 25.5 25.1 25.3 25.1 25.2 25.2 25.2 25.3 25.3 25.3 25.3 25.1 25.0 25.1 25.1 25.1 24.9 25.3 25.2 25.3 25.0 25.2 25.1 25.1 25.2 24.8\n2024-11-06-12-33-42 25.1 25.3 25.2 25.1 25.0 25.0 25.1 25.0 25.0 25.2 25.3 25.3 25.2 25.3 25.3 25.3 25.2 25.0 25.0 24.9 25.0 25.3 25.2 24.9 25.3 25.2 24.9 25.3 25.0 25.2 24.9 24.9 25.1 25.1 24.9 25.2 24.9 25.1 25.1 25.1 25.2 25.0 25.0 25.0 25.0 25.1 25.2 25.0 24.9 25.0 25.2 25.1 24.9 25.0 25.0 25.2 25.1 25.2 25.1 25.1 24.9 24.9 25.1 25.1 24.9 24.9 25.2 25.0 24.9 24.8 25.1 25.0 25.0 24.8 24.9 25.0 25.0 25.0 25.0 25.0\n2024-11-06-12-35-42 29.5 29.3 29.3 29.1 29.1 29.4 29.0 29.1 29.1 28.9 29.0 28.8 28.8 28.7 28.8 28.8 28.6 28.4 28.6 28.3 28.5 28.4 28.2 28.0 28.0 28.0 27.9 28.1 28.1 27.7 27.9 27.7 27.8 27.8 27.5 27.3 27.6 27.5 27.5 27.1 27.1 27.1 27.0 26.9 27.0 26.9 27.0 26.8 26.6 26.7 26.5 26.6 26.5 26.3 26.6 26.6 26.5 26.4 26.4 26.2 26.3 26.1 26.1 25.8 25.9 25.7 25.7 25.8 25.6 25.6 25.7 25.4 25.5 25.2 25.4 25.3 25.4 25.3 25.2 25.0 24.8\n2024-11-06-12-37-42 32.3 32.4 32.1 32.0 31.8 31.7 31.5 31.4 31.4 30.8 30.9 30.5 30.7 30.5 30.4 29.9 30.1 29.6 29.6 29.6 29.2 29.1 28.9 28.9 28.6 28.3 28.4 28.0 28.0 27.8 27.9 27.6 27.2 27.1 26.9 26.8 26.6 26.7 26.4 26.4 25.9 25.8 25.9 25.4 25.4 25.2 25.1 25.1\n2024-11-06-12-39-42 29.3 29.4 29.5 29.2 29.2 29.1 29.3 29.0 29.0 29.1 29.0 28.9 29.0 28.9 28.7 28.8 28.7 28.7 28.3 28.4 28.5 28.5 28.3 28.2 28.1 28.0 28.1 28.2 27.8 28.0 27.8 27.9 27.7 27.9 27.5 27.4 27.7 27.6 27.4 27.5 27.2 27.1 27.2 27.4 27.2 27.1 26.9 26.9 26.8 26.7 26.8 26.6 26.7 26.4 26.6 26.5 26.6 26.3 26.4 26.2 26.1 26.2 26.1 25.9 26.2 26.2 26.1 25.7 25.8 25.8 25.7 25.6 25.7 25.6 25.5 25.4 25.2 25.2 25.3 25.1 25.2 25.1 24.9 24.9\n2024-11-06-12-40-42 33.8 33.5 33.7 33.3 33.3 33.1 32.7 32.6 32.6 32.3 32.2 32.1 31.7 31.7 31.5 31.4 31.3 30.9 30.8 30.5 30.5 30.1 30.1 29.8 29.7 29.5 29.6 29.4 29.0 29.1 28.6 28.4 28.3 28.2 28.2 27.8 27.7 27.4 27.3 27.3 27.2 27.0 26.5 26.7 26.4 26.0 26.1 26.0 25.8 25.4 25.3 25.2 24.8";
+  //Serial.println("Attempting to send data...");
   if(WiFi.status() == WL_CONNECTED){
     // start connection
     client.begin(HOST_NAME+PATH_NAME);
@@ -62,7 +73,7 @@ void send_TestData(String Data) {
     client.addHeader("Content-Type", "text/plain");
     client.addHeader("Connection", "close");
     client.addHeader("direction", "0");
-    client.addHeader("custom-text", "MDR Demo Button");
+    client.addHeader("custom-text", "CDR Testing Data");
 
     // Send POST request
     int httpResponseCode = client.POST(Data);
@@ -70,7 +81,77 @@ void send_TestData(String Data) {
     // Check the HTTP response
     if (httpResponseCode > 0) {
       String response = client.getString();
+      //Serial.println("Response Code: " + String(httpResponseCode));
+      //Serial.println("Response: " + response);
+    } else {
+      //Serial.println("Error in sending POST: " + String(httpResponseCode));
     }
     client.end();
+
+  } else {
+    //Serial.println("Connection failed.");
+  }
+}
+
+void setup_mode(){
+
+  delay(1000);
+  //Serial.begin(115200);
+  //Serial.println();
+
+  WiFi.config(localIP, gateway, subnet);
+  WiFi.softAP(ServerSSID, ServerPassword);
+  
+  server.on("/", HTTP_POST, handlePost);
+  server.begin();
+  //Serial.println("Server Started");
+
+  delay(500);
+
+  //Serial.println("IP Address: " + WiFi.softAPIP().toString());
+  //Serial.print("Local IP: ");
+  //Serial.println(WiFi.localIP());
+  //Serial.print("WiFi Status: ");
+  //Serial.println(WiFi.status());
+}
+
+bool connectToWiFi(String ssid, String password) {
+  //Serial.println("Attempting to connect to WiFi...");
+  
+  WiFi.softAPdisconnect(true);
+  WiFi.disconnect();
+  WiFi.mode(WIFI_OFF);
+  delay(3000);
+  WiFi.mode(WIFI_STA);
+  delay(1000);
+  WiFi.begin(ssid.c_str(), password.c_str());
+
+  int attempts = 0;
+  while (WiFi.status() != WL_CONNECTED && attempts < 10) {  // Try for ~5 seconds
+    delay(500);
+    //Serial.print(".");
+    attempts++;
+  }
+
+  //Serial.print("Connecting to: ");
+  //Serial.println(ssid);
+  //Serial.print("With password: ");
+  //Serial.println(password);
+  //Serial.print("WiFi Status Code: ");
+  //Serial.println(WiFi.status());
+  
+  if (WiFi.status() == WL_CONNECTED) {
+    //Serial.println("\nConnected to WiFi!");
+    //Serial.print("IP Address: ");
+    //Serial.println(WiFi.localIP());
+    return true;
+  } else {
+    //Serial.println("\nFailed to connect to WiFi.");
+    WiFi.softAP(ServerSSID, ServerPassword);
+  
+    server.on("/", HTTP_POST, handlePost);
+    server.begin();
+    //Serial.println("Server Restarted after invalid credentials sent");
+    return false;
   }
 }
